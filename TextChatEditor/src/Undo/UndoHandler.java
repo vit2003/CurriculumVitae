65ee26343aff43518f47774285dc29981a906be1
vit2003 <vinhle2311@gmail.com>
2020-07-20 12:43:39 +0700

    This project is about text editor

---
+++ TextChatEditor/src/Undo/UndoHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Undo;
+
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UndoHandler implements UndoableEditListener{
+
+    @Override
+    public void undoableEditHappened(UndoableEditEvent arg0) {
+    
+    }
+    
+}
